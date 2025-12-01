a86783433fb60419008da4e9d838e11475a5fb42
Gaming Insights <insightsgaming0@gmail.com>
2025-12-01 13:06:18 +0000

    info dump2 update

--- models/staging/raw/stg_raw__info_dump2.sql
+++ models/staging/raw/stg_raw__info_dump2.sql
@@ -10,6 +10,46 @@ renamed as (
 
     select
 
+        release_date,
+        achievements,
+        movies,
+        support_info,
+        screenshots,
+        genres,
+        categories,
+        platforms,
+        package_groups,
+        about_the_game,
+        website,
+        demos,
+        ext_user_account_notice,
+        drm_notice,
+        legal_notice,
+        linux_requirements,
+        developers,
+        mac_requirements,
+        supported_languages,
+        fullgame,
+        metacritic,
+        detailed_description,
+        header_image,
+        required_age,
+        is_free,
+        type,
+        packages,
+        price_overview,
+        publishers,
+        background,
+        name,
+        content_descriptors,
+        recommendations,
+        short_description,
+        pc_requirements,
+        reviews,
+        controller_support,
+        steam_appid,
+        dlc
+
     from source
 
 )
